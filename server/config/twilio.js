a87fc4748f4e25c0c87ba6c56d763bfddca94846
cwhwang1986 <cwhwang1986@gmail.com>
2015-05-21 16:06:36 -0700

    Update twilio.js

--- server/config/twilio.js
+++ server/config/twilio.js
@@ -4,7 +4,6 @@
 
 //require the Twilio module and create a REST client 
 var client = require('twilio')(accountSid, authToken);
-client.mobileNumber = '+16502242246';
-client.twillioNumber = '+14153196800';
+
 //Expoet the twillio module
 module.exports = client;
