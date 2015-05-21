971ea2ab7977b039d111b473a9baaf19f7370fc9
cwhwang1986 <cwhwang1986@gmail.com>
2015-05-21 16:06:24 -0700

    Update twilio.js

--- server/config/twilio.js
+++ server/config/twilio.js
@@ -1,6 +1,5 @@
 // Twilio Credentials 
-var accountSid = 'ACac0d6af5ad2234e941a0e76f2df365d6'; 
-var authToken = '550e5de193bdfd3591026bae8bdeb547'; 
+
 
 
 //require the Twilio module and create a REST client 
